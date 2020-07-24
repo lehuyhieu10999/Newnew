b92dfe1c495256005836e43c8de3e34096d97e5f
huyhieu <lehuyhieu1099@gmail.com>
2020-07-24 21:14:21 +0700

    bt

---
+++ JsonEx/Ex1/Program.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+
+namespace JsonEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"C:\Users\ADMIN\Desktop\Huy Hiệu\Newnew\JsonEx\JsonEx\Data\input.json";
+            var result = new Data();
+           
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                var data = sr.ReadToEnd();
+                result = JsonConvert.DeserializeObject<Data>(data);
+            }
+           
+            
+            // 1.
+            var response = new Response()
+            {
+                objects = new List<SumObject>()
+            };
+            
+            SumObject sumObject = new SumObject()
+            {
+                sum_1 = result.objects[0].a + result.objects[0].b + result.objects[0].c,
+                sum_2 = result.objects[1].a + result.objects[1].b + result.objects[1].c,
+                sum_3 = result.objects[2].a + result.objects[2].b + result.objects[2].c
+            };
+
+            var outFilePath = @"C:\Users\ADMIN\Desktop\Huy Hiệu\Newnew\JsonEx\JsonEx\Data\output1.json";
+            using (StreamWriter sw = File.CreateText(outFilePath))
+            {
+                var data = JsonConvert.SerializeObject(sumObject);
+                sw.Write(data);
+            }
+           
+            
+            // 2.
+            foreach(var item in result.objects)
+            {
+
+                item.a *= 2;
+                item.b *= 2;
+                item.c *= 2;
+
+            }
+            var outFilePath2 = @"C:\Users\ADMIN\Desktop\Huy Hiệu\Newnew\JsonEx\JsonEx\Data\output2.json";
+            using (StreamWriter sw = File.CreateText(outFilePath2))
+            {
+                var data = JsonConvert.SerializeObject(result);
+                sw.Write(data);
+            }
+
+
+        }
+        class Data
+        {
+            public List<Object> objects { get; set; }
+        }
+        public class Object
+        {
+            public int a { get; set; }
+            public int b { get; set; }
+            public int c { get; set; }
+        }
+        public class SumObject
+        {
+            public int sum_1 { get; set; }
+            public int sum_2 { get; set; }
+            public int sum_3 { get; set; }
+        }
+       
+        // trả về của data
+        public class Response
+        {
+            public List<SumObject> objects { get; set; }
+        }
+       
+    }
+}
---
+++ JsonEx/Ex2/Program.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filePath = @"C:\Users\ADMIN\Desktop\Huy Hiệu\Newnew\JsonEx\Ex2\Data\data.json";
+            var outFilePath = @"C:\Users\ADMIN\Desktop\Huy Hiệu\Newnew\JsonEx\Ex2\Data\Outcome.json";
+            var result = new Data();
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                var data = sr.ReadToEnd();
+                result = JsonConvert.DeserializeObject<Data>(data);
+            }
+
+            var response = new ResponseData()
+            {
+                students = new List<ResStudent>()
+            };
+            
+            foreach (var std in result.students)
+            {
+                response.students.Add(new ResStudent()
+                {
+                    Id = std.Id,
+                    Name = std.Name,
+                    Gender = std.Gender,
+                    Class = std.Class,
+                    subjects = std.subjects,
+                    average = std.AverageScore(),
+                });
+            }
+            for (int i =0; i < response.students.Count - 1; i++)
+            {
+                if (response.students[i].average < response.students[i + 1].average)
+                {
+                    var temp = response.students[i];
+                    response.students[i] = response.students[i + 1];
+                    response.students[i + 1] = temp;
+                } 
+            }
+            using (StreamWriter sw = File.CreateText(outFilePath))
+            {
+                var data = JsonConvert.SerializeObject(response);
+                sw.Write(data);
+            }
+        }
+        public class Subject
+        {
+            public string name { get; set; }
+            public int score { get; set; }
+
+        }
+
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Gender { get; set; }
+            public string Class { get; set; }
+            public List<Subject> subjects { get; set; }
+            public float AverageScore()
+            {
+                float total = 0;
+                foreach (var item in subjects)
+                {
+                    if (item.name == "Math")
+                    {
+                        total += item.score * 2;
+                    }
+                    else
+                    {
+                        total += item.score;
+                    }
+                }
+                return total / (subjects.Count + 1);
+            }
+            public override string ToString()
+            {
+                return $"{Id}\t\t{Name}\t\t{Gender}\t\t{Class}\t\t{subjects[0].score}\t\t{subjects[1].score}\t\t{subjects[2].score}\t\t{AverageScore()}";
+            }
+
+        }
+
+        public class Data
+        {
+            public List<Student> students { get; set; }
+
+        }
+
+        public class ResponseData
+        {
+            public List<ResStudent> students { get; set; }
+        }
+        public class ResStudent : Student
+        {
+            public float average { get; set; }
+            public string rank => Rank();
+            public string Rank()
+            {
+                if (average >= 9)
+                {
+                    return "Xuat sac";
+                }
+                if (average >= 8)
+                {
+                    return "Gioi";
+                }
+                if (average >= 6.5)
+                {
+                    return "Kha";
+                }
+                if (average >= 5)
+                {
+                    return "TB";
+                }
+                return "Yeu";
+            }
+        }
+
+    }
+}
